765c928a4c2306720149583cd28023f52972f778
andres17f <43790317+andres17f@users.noreply.github.com>
2018-11-10 19:00:53 +0100

    Lista sin metodos hecho.

--- lista.js
+++ lista.js
@@ -7,11 +7,11 @@
 
 function addNumber(num){
 	var error = document.getElementById ("error");
-	var queue = document.getElementById ("queue");
+	var list = document.getElementById ("list");
 	error.innerHTML = "";  
  	try {
 	 	add(NUMBERS_QUEUE,num);
-	 	queue.innerHTML = toString(NUMBERS_QUEUE);
+	 	list.innerHTML = toString(NUMBERS_QUEUE);
  	} catch (err) {
  		error.innerHTML = err;
 	 }	
@@ -19,61 +19,61 @@ function addNumber(num){
 
  function pollNumber (){
 	var error = document.getElementById ("error");
-	var queue = document.getElementById ("queue");
+	var list = document.getElementById ("list");
 	error.innerHTML = "";  
  	try {
 	 	poll(NUMBERS_QUEUE);
-	 	queue.innerHTML = toString(NUMBERS_QUEUE);
+	 	list.innerHTML = toString(NUMBERS_QUEUE);
  	} catch (err) {
  		error.innerHTML = err;
  	}		
 }
 
-/* queue Functions */
+/* list Functions */
  var MAX_ELEM_QUEUE = 5; 
  function create(){
- 	var queue = [];
+ 	var list = [];
  	for (var i=0; i<MAX_ELEM_QUEUE; i++){
- 		queue[i] = Number.NaN;
+ 		list[i] = Number.NaN;
  	}
- 	return queue;
+ 	return list;
  } 
 
- function isEmpty(queue){
+ function isEmpty(list){
  	var isEmpty = false;
- 	if (isNaN(queue[0])){
+ 	if (isNaN(list[0])){
  		isEmpty = true;
  	}
  	return isEmpty;
  } 
 
- function isFull(queue){
+ function isFull(list){
  	var isFull = false;
- 	if (!isNaN(queue[MAX_ELEM_QUEUE-1])){
+ 	if (!isNaN(list[MAX_ELEM_QUEUE-1])){
  		isFull = true;
  	}
  	return isFull;
  } 
 
- function size(queue){
+ function size(list){
  	var length = 0;
- 	while (length<MAX_ELEM_QUEUE && !isNaN(queue[length])){
+ 	while (length<MAX_ELEM_QUEUE && !isNaN(list[length])){
  		length++;
  	}
  	return length;
  } 
 
- function add(queue,elem){
+ function add(list,elem){
  	elem = parseInt(elem);
  	if (isNaN(elem)) {
  		throw "The element is not a number";
  	}
- 	if (!isFull(queue)){
- 		queue[size(queue)] = elem;
+ 	if (!isFull(list)){
+ 		list[size(list)] = elem;
  	} else {
- 		throw "The queue is Full. You can't put the element in it";
+ 		throw "The list is Full. You can't put the element in it";
  	}
- 	return size(queue);
+ 	return size(list);
  } 
 
  /*Añade un elemento en la posicion de la lista que especifiquemos,
@@ -81,45 +81,40 @@ function addNumber(num){
  es mayor que el tamaño de la lista y lo añade en el ultimo sitio.
  En cambio si la posicion esta en medio desplaza los valores de por
 ejemplo la posicion 3 a la 4 y añadimos el elemento en la poscion 3.*/
- function addAt(queue,elem,index){
+ function addAt(list,elem,index){
 	elem = parseInt(elem);
-	var lengt = size(queue);
+	var lengt = size(list);
 	if (isNaN(elem)) {
 		throw "The element is not a number";
 	}
-	if (index>(size(queue))) {
+	if (index>(size(list))) {
 		throw "The index is outside the limits of the list";
 	}
-	if (!isFull(queue)){
-		queue[size(queue)] = elem;
+	if (isFull(list)){
+		throw "The list is Full. You can't put the element in it";
 	} else {
-		throw "The queue is Full. You can't put the element in it";
+		list[index]=elem;
+		for (var i=index; i<length ; i++) {
+			list[i]= list[i+1];
+		} 
+		
 	}
-	if (index>length) {
-		queue[size(queue)] = elem;
-	} else { 	
-		var length = size(queue);	
-		for (var i=index; i<lengt ; i++) {
-			queue[i+1]= queue[i];
-		}
-			queue[i] = elem;
-	}
-	return size(queue);
+	return size(list);
  } 
 
- function get(queue,index){
-	var elem = queue[index];
+ function get(list,index){
+	var elem = list[index];
 	return elem;
  }
 
- function toString(queue){
+ function toString(list){
 	var str = "";
-	if (!isEmpty(queue)){
-		var length = size(queue);	
+	if (!isEmpty(list)){
+		var length = size(list);	
 		for (var i=0; i<length-1;i++){
-			str = str + queue[i] + " - ";
+			str = str + list[i] + " - ";
 		} 		 		
-		str = str + queue[i]; 		
+		str = str + list[i]; 		
 	} 	
 	return str;
  } 
@@ -128,15 +123,15 @@ ejemplo la posicion 3 a la 4 y añadimos el elemento en la poscion 3.*/
  y la recorre empezando por el principio, cuando el valor de la lista
  es igual al que hemos pedido nos cambia el valor de position y nos
  lo devuelve.*/
- function indexOf(queue,elem){
+ function indexOf(list,elem){
 	var position = -1;
 	elem = parseInt(elem);
 	if (!isNaN(elem)) {
-		if (!isEmpty(queue)){
-			var length = size(queue);	
+		if (!isEmpty(list)){
+			var length = size(list);	
 			var i=0;
 			while (i<length && position === -1){
-				if (queue[i] === elem) {
+				if (list[i] === elem) {
 					position = i;
 				}
 				i++;
@@ -151,15 +146,15 @@ ejemplo la posicion 3 a la 4 y añadimos el elemento en la poscion 3.*/
 /*Tiene un funcionamiento parecido a la funcion anterior solo que aqui
 comienza a buscar por el final, lo hacemo asignando el valor final
 que seria el length-1 y recorremos a la inversa hasta el 0.*/
- function lastindexOf(queue,elem){
+ function lastindexOf(list,elem){
 	var position = -1;
 	elem = parseInt(elem);
 	if (!isNaN(elem)) {
-		if (!isEmpty(queue)){
-			var length = size(queue);	
+		if (!isEmpty(list)){
+			var length = size(list);	
 			var i = (length-1);
 			while (i>=0 && position === -1){
-				if (queue[i] === elem) {
+				if (list[i] === elem) {
 					position = i;
 				}
 				i--;
@@ -171,36 +166,36 @@ que seria el length-1 y recorremos a la inversa hasta el 0.*/
 	return position;
  }
 
- function capacity(queue){
+ function capacity(list){
 	return MAX_ELEM_QUEUE;
  }  
 
- function clear(queue){
+ function clear(list){
 	var elem = Number.NaN;
-	if (!isEmpty(queue)){
-		var length = size(queue);	
+	if (!isEmpty(list)){
+		var length = size(list);	
 		for (var i=0; i<length;i++){
-			queue[i] = elem;
+			list[i] = elem;
 		} 		 		 		
 	} 	
  } 
 
- function firstElement(queue){
+ function firstElement(list){
 	var first;
-	if (!isEmpty(queue)){
-		first = queue[0]; 		
+	if (!isEmpty(list)){
+		first = list[0]; 		
 	} else {
-		throw "The queue is empty.";
+		throw "The list is empty.";
 	}
 	return first;
  } 
 
- function lastElement(queue){
+ function lastElement(list){
 	var last;
-	if (!isEmpty(queue)){
-		last = queue[size(queue)-1]; 		
+	if (!isEmpty(list)){
+		last = list[size(list)-1]; 		
 	} else {
-		throw "The queue is empty.";
+		throw "The list is empty.";
 	}
 	return last;
  } 
@@ -209,15 +204,15 @@ que seria el length-1 y recorremos a la inversa hasta el 0.*/
  y nos quedamos con el elemento que se quiere borrar para mostrarlo,
  y despues se recorre el array desde esa posicion hasta el final
  machando el valor de la pos 3 por el de la 4 y asi sucesivamente.*/
- function remove(queue,index){
+ function remove(list,index){
 	var pos = index;
-	var length = size(queue);
+	var length = size(list);
 	var elem = 0;
 	if (pos<length) { 
-		var length = size(queue);	
-		elem =queue[pos];
+		var length = size(list);	
+		elem =list[pos];
 		for (var i=pos; i<length ; i++) {
-			queue[i]= queue[i+1];
+			list[i]= list[i+1];
 		} 	 		
 	} else {
 		throw "The index is outside the limits of the list";
@@ -227,16 +222,16 @@ que seria el length-1 y recorremos a la inversa hasta el 0.*/
 
  /*Recorre el array, si encuentra el elemento que deseamos borrar,
  reescribe el array para machacarlo */
- function removeElement(queue,elem) {
-	elemento= ParseInt(elem); 
-	var length = size(queue);
-	var enco = FALSE;
+ function removeElement(list,elem) {
+	var elemento = parseInt(elem); 
+	var length = size(list);
+	var enco = false;
 	if (!isNaN(elemento)) { 
 		for (var i=0; i<length ; i++) {
-			if (queue(i) === elemento) {
-				encontrado = TRUE;
+			if (list[i] === elemento) {
+				enco = true;
 				for (var j=i; j<length ; j++) {
-					queue[j]= queue[j+1];
+					list[j]= list[j+1];
 				} 
 			}
 		} 	 		
@@ -246,52 +241,53 @@ que seria el length-1 y recorremos a la inversa hasta el 0.*/
 	return enco;
  }
 
- function set(queue,elem,index){
+ function set(list,elem,index){
 	elem = parseInt(elem);
-	var lengt = size(queue);
 	var susti = 0;
 	if (isNaN(elem)) {
 		throw "The element is not a number";
 	}
-	if (index>(size(queue))) {
+	if (index>(size(list))) {
 		throw "The index is outside the limits of the list";
 	}
-	susti = queue[index];
-	queue[index] = elem;
+	susti = list[index];
+	list[index] = elem;
 	return susti;
  } 
 
 /*function de testeo*/
 
  function testqueue(){
- 	//var queue = create (); 	
- 	var queue=[]; 	
- 	console.log ("Capacidad: " + capacity(queue));
- 	console.log("Es vacía: " + isEmpty(queue));
- 	console.log("Longitud: " + size(queue));
+ 	//var list = create (); 	
+ 	var list=[]; 	
+ 	console.log ("Capacidad: " + capacity(list));
+ 	console.log("Es vacía: " + isEmpty(list));
+ 	console.log("Longitud: " + size(list));
 
  	try {
 	 	for (var i=0; i<MAX_ELEM_QUEUE; i++){
-	 		console.log("Nº de elementos: " + add(queue,i*10));
+	 		console.log("Nº de elementos: " + add(list,i*10));
 	 	}
-	 	add(queue,i); //It will generate an exception.
+	 	add(list,i); //It will generate an exception.
  	} catch (err) {
  		console.log(err);
  	}
 
- 	console.log ("The full queue: " + toString(queue));	 	
- 	console.log ("The first element queue: " + firstElement(queue));
- 	console.log ("The last element queue: " + lastElement(queue));
+ 	console.log ("The full queue: " + toString(list));	 	
+ 	console.log ("The first element queue: " + firstElement(list));
+ 	console.log ("The last element queue: " + lastElement(list));
 	 		 	
- 	//clear(queue);
+ 	//clear(list);
 
-	 console.log ("Prueba de remove: " + remove(queue,2));
-	 /*console.log ("Prueba de removeElement: " + removeElement(queue,30));*/
-	 console.log ("Prueba de set: " + set(queue,100,3));
-	 console.log ("Prueba de indexOf: " + indexOf(queue,10));
-	 console.log ("Prueba de lastindexoF: " + lastindexOf(queue,30));
-	 console.log ("Prueba de addAt: " + addAt(queue,19,2));
+	 console.log ("Prueba de remove: " + remove(list,2));
+	 console.log ("Lista antes: " + toString(list));
+	 console.log ("Prueba de removeElement: " + removeElement(list,30));
+	 console.log ("Lista despues: " + toString(list));
+	 console.log ("Prueba de set: " + set(list,100,3));
+	 console.log ("Prueba de indexOf: " + indexOf(list,10));
+	 console.log ("Prueba de lastindexoF: " + lastindexOf(list,30));
+	 console.log ("Prueba de addAt: " + addAt(list,19,2));
 
- 	 console.log ("The queue: " + toString(queue));	 	
+ 	 console.log ("The List: " + toString(list));	 	
  } 
 window.onload = testqueue;
